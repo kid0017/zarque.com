a19f8c0a1c2248ddc5b4b7dbe434907985c6dffd
kid0017 <nickytem368@gmail.com>
2025-06-17 14:37:07 +0530

    Add files via upload
    Signed-off-by: kid0017 <nickytem368@gmail.com>

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function toggleSystemBar() {
   const bar = document.getElementById("systemBar");
   bar.style.display = bar.style.display === "flex" ? "none" : "flex";
 }
+
+function glitchTransition(e) {
+  e.preventDefault();
+  document.body.classList.add("glitch-out");
+  setTimeout(() => {
+    window.location.href = e.target.href;
+  }, 400);
+}
